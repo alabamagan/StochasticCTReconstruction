a257fb3320c22ea748129a3c3350778463277498
teracamo <fromosia@gmail.com>
2017-07-02 17:42:45 +0800

    Move some code to GaussianMixtureModel.py

--- Algorithm/GaussianMixtureFitter.py
+++ Algorithm/GaussianMixtureFitter.py
@@ -41,6 +41,7 @@ def RemoveDistinctValues(input, threshold):
 
     return input
 
+
 def GaussianSmooth(input, inDomain, radius):
     """
     Descriptions
@@ -239,62 +240,25 @@ def SKLearnFitter(inData, numOfGaussians=[2]):
     return [(parameters[i*3+0], parameters[i*3+1], np.sqrt(parameters[i*3+2])) for i in xrange(numOfGaussians[np.argmin(AIC)])]
 
 
-def GaussianComponenetMatching(GMM1, GMM2):
-    """
-    Descriptions
-    ------------
+def SortGMMs(GMMList, groupParameters=False):
+    raise NotImplemented
+    #=======================================
+    # Preprocessing
+    #=======================================
+    numOfGMMs = len(GMMList)
 
-      This function pair up Gaussian components of the input based on their distance.
-
-    Example:
-    --------
-
-    >>> GMM1 = [(10, 1, 2), (20, -3, 4)]
-    >>> GMM2 = [(2, 1.3, 4), (5, -2, 4)]
-    >>> GMMPairs = GaussianComponenetMatching(GMM1, GMM2)
-    >>> print GMMPairs # [[0,0,0.3], [1,1,1]]
-
-    :param GMM1:    Gaussian mixture model 1
-    :param GMM2:    Gaussian mixture model 2
-    :return:        Pairs inform of [[GMM1 index, GMM2 index, distance],...]
-    """
-
-    #=============================================================
-    # Error check
-    #=============================================================
-    if len(GMM1) != len(GMM2):
-        raise ArithmeticError("Two input has different size.")
-
-
-    #=============================================================
-    # Pair up gaussians
-    #=============================================================
-    gaussiansPair = []
-    s = len(GMM1)
-    paired = []
-
-    #----------------------------------------
-    # Starts pairing the two GMMs
-    try:
-        for i in xrange(s):
-            mu_1 = GMM1[i][1]
-
-            pairIndex = -1
-            dist = 1E10
-            for j in unpaired:
-                mu_2 = GMM2[j][1]
-                d = np.abs(mu_1 - mu2)
-
-                # Replace index if the distance is smaller
-                if d < dist and paired.count(j) == 1:
-                    dist = d
-                    pairIndex= j
-
-            # Pair only when an index is found
-            if (pairIndex != -1):
-                gaussiansPair.append([i,j, pairIndex])
-                paired.append(j)
-    except(IndexError):
-        raise IndexError("Gaussian mixtures has wrong number of parameters.")
-
-    return paired
+    #========================================
+    # Identify GMM paris
+    #---------------------------------------------------
+    # Make sure the Gaussian components are paired up
+    for i in xrange(GMMList):
+        for j in xrange(GMMList) and j != i:
+            # Skip if comparing with itself
+            if i == j:
+                continue
+            else:
+                pairs = GaussianComponenetMatching(GMMList[i],
+                                                   GMMList[j])
+    #========================================
+    #
+    #========================================
