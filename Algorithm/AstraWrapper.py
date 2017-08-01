2da8300e2a5eb7f905ce41c352fa9a67228b7f53
lwong <fromosia@gmail.com>
2017-08-01 16:33:23 +0800

    Add circular mask option during reconstruction. GRAM optimization

--- Algorithm/AstraWrapper.py
+++ Algorithm/AstraWrapper.py
@@ -1,5 +1,6 @@
 import astra
 import numpy as np
+import SimpleITK as sitk
 
 class Projector(object):
     def __init__(self):
@@ -63,21 +64,28 @@ class Projector(object):
             astra.data3d.delete(self._proj_id)
         self._proj_id, self._proj_data = astra.create_sino3d_gpu(self._imageVol, proj_geom, self._vol_geom)
         self._proj_array = astra.data3d.get(self._proj_id)
+
+        # Release GRAM
+        astra.projector.delete(self._proj_id)
+        astra.data3d.delete(self._imageVol)
         pass
 
 class Reconstructor(object):
     def __init__(self):
-        self._rec_id = 0
-        self._vol_geom = 0
-        self._sino_id = 0
-        self._alg_id = 0
+        self._rec_id = -1
+        self._vol_geom = -1
+        self._sino_id = -1
+        self._alg_id = -1
+        self._mask_id = -1
         pass
 
     def __delete__(self, instance):
-        if self._rec_id:
+        if self._rec_id >= 0:
             astra.data3d.delete(self._rec_id)
-        if self._sino_id:
+        if self._sino_id >= 0:
             astra.data3d.delete(self._sino_id)
+        if self._mask_id >= 0:
+            astra.data3d.delete(self._mask_id)
         pass
 
 
@@ -90,24 +98,46 @@ class Reconstructor(object):
 
         :return:
         """
-        if (self._rec_id):
+        if (self._rec_id >= 0):
             astra.data3d.delete(self._rec_id)
 
         if (kwargs.has_key('vol_geom')):
             vol_geom = kwargs['vol_geom']
+            xSize = vol_geom['GridColCount']
+            ySize = vol_geom['GridRowCount']
+            zSize = vol_geom['GridSliceCount']
         elif (kwargs.has_key('imageShape')):
             imshape = kwargs['imageShape']
             xSize = imshape[2]
             ySize = imshape[1]
             zSize = imshape[0]
             vol_geom = astra.create_vol_geom(ySize, xSize, zSize)
+        else:
+            raise AssertionError("Argument must contain either 'vol_geom' or 'imageShape'" )
+
+        #--------------------------------------------------------------------
+        # Create Circular mask
+        if (kwargs.has_key('circle_mask')):
+            if (kwargs['circle_mask']):
+                center = np.array([zSize, ySize, xSize])/2.
+                meshgridY, meshgridZ, meshgridX = np.meshgrid(xrange(ySize),
+                                                              xrange(zSize),
+                                                              xrange(xSize))
+                # for unknown reasons, meshgrid will transpose along axis 2
+                mask = (meshgridY - center[1])**2 + \
+                       (meshgridX - center[2])**2 < ((ySize+1)/2.)**2
+                im = np.zeros([zSize, ySize, xSize])
+                im[mask] = 255
+                im = sitk.GetImageFromArray(im)
+                # sitk.WriteImage(im, "../TestData/mask.nii.gz")
+                self._mask_id = astra.data3d.create('-vol', vol_geom, mask)
 
         self._vol_geom = vol_geom
         self._rec_id = astra.data3d.create('-vol', vol_geom)
 
 
     def SetInputSinogram(self, sino, thetas):
-        if(self._sino_id):
+        if(self._sino_id >= 0):
             astra.data3d.delete(self._sino_id)
 
         if (type(sino) == np.ndarray):
@@ -115,7 +145,6 @@ class Reconstructor(object):
             xSize = imshape[1]
             ySize = imshape[2]
             zSize = imshape[0]
-            print imshape
             proj_geom = astra.create_proj_geom('parallel3d', 1., 1.,
                                                zSize, ySize,
                                                thetas)
@@ -142,9 +171,19 @@ class Reconstructor(object):
         cfg = astra.astra_dict('CGLS3D_CUDA')
         cfg['ReconstructionDataId'] = self._rec_id
         cfg['ProjectionDataId'] = self._sino_id
+        if (self._mask_id >= 0):
+            cfg['option'] = {}
+            cfg['option']['ReconstructionMaskId'] = self._mask_id
 
         self._alg_id = astra.algorithm.create(cfg)
         astra.algorithm.run(self._alg_id, iterations)
 
         output = astra.data3d.get(self._rec_id)
+
+        # Release GRAM
+        astra.algorithm.delete(self._alg_id)
+        astra.data3d.delete(self._sino_id)
+        astra.data3d.delete(self._rec_id)
+        if (self._mask_id >= 0):
+            astra.data3d.delete(self._mask_id)
         return output
